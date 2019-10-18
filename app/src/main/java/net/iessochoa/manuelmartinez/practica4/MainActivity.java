016d927e22ac609b550bf42fcadaa0d1ca0cea17
ManuelM <descom666999@gmail.com>
2019-10-18 20:42:41 +0200

    terminado acerca de

--- app/src/main/java/net/iessochoa/manuelmartinez/practica4/MainActivity.java
+++ app/src/main/java/net/iessochoa/manuelmartinez/practica4/MainActivity.java
@@ -1,31 +1,86 @@
 package net.iessochoa.manuelmartinez.practica4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+    Button btAcercade;
+    Button btOrdenar;
+    Button btAnyadir;
+
+    public void MensajeAcercade(){
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(this);
+        dialogo.setTitle("Acerca de ");// titulo y mensaje
+        dialogo.setMessage("Práctica 4\n"+
+                "Manuel Martínez Serrano\n"+
+                "Licencia cc (Creative Commons)");
+
+
+        // agregamos botón Ok y su evento
+        dialogo.setPositiveButton(android.R.string.yes ,
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Qué hacemos en caso ok ....
+                        onDestroy();
+                    } });
+
+        /*dialogo.setNegativeButton(android.R.string.no ,
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Qué hacemos en caso cancel ......
+
+                    } });*/
+        dialogo.show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btAcercade=findViewById(R.id.btAcercade);
+        btAnyadir=findViewById(R.id.btAnyadir);
+        btOrdenar=findViewById(R.id.btOrdenar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btAcercade:
+                MensajeAcercade();
+                break;
+            case R.id.btAnyadir:
+
+                break;
+            case R.id.btOrdenar:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
