59964b8cbaff9bbd3638956d220549ee01839d5e
ManuelM <descom666999@gmail.com>
2019-10-18 17:01:47 +0200

    Initial commit

---
+++ app/src/main/java/net/iessochoa/manuelmartinez/practica4/MainActivity.java
@@ -0,0 +1,32 @@
+package net.iessochoa.manuelmartinez.practica4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
