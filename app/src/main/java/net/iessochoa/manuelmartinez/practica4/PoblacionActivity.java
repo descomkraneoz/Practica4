6274cd302296417abd7272a7867a4d6c26e4d500
descomkraneoz <descom666999@gmail.com>
2019-10-23 13:15:36 +0200

    probando spinners, parece que funcionan

--- app/src/main/java/net/iessochoa/manuelmartinez/practica4/PoblacionActivity.java
+++ app/src/main/java/net/iessochoa/manuelmartinez/practica4/PoblacionActivity.java
@@ -83,23 +83,29 @@ public class PoblacionActivity extends AppCompatActivity implements AdapterView.
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        int[] localidades = {R.array.localidades_alicante, R.array.localidades_alava, R.array.localidades_albacete, R.array.localidades_almeria,
+        int[] localidades = {R.array.localidades_albacete, R.array.localidades_alava, R.array.localidades_alicante, R.array.localidades_almeria,
                 R.array.localidades_asturias, R.array.localidades_avila, R.array.localidades_badajoz, R.array.localidades_barcelona,
                 R.array.localidades_burgos, R.array.localidades_caceres, R.array.localidades_cadiz, R.array.localidades_cantabria,
                 R.array.localidades_castellon, R.array.localidades_ceuta, R.array.localidades_ciudadReal, R.array.localidades_cordoba,
                 R.array.localidades_cuenca, R.array.localidades_gerona, R.array.localidades_granada, R.array.localidades_guadalajara,
                 R.array.localidades_guipuzcoa, R.array.localidades_huelva, R.array.localidades_huesca, R.array.localidades_islasBaleares,
                 R.array.localidades_jaen, R.array.localidades_laCoruna, R.array.localidades_laRioja, R.array.localidades_lasPalmas,
-                R.array.localidades_leon, R.array.localidades_lerida, R.array.localidades_lugo, R.array.localidades_madrid,
-                R.array.localidades_orense, R.array.localidades_palencia, R.array.localidades_pontevedra, R.array.localidades_salamanca,
+                R.array.localidades_lerida, R.array.localidades_leon, R.array.localidades_lugo, R.array.localidades_madrid, R.array.localidades_malaga,
+                R.array.localidades_melilla, R.array.localidades_murcia, R.array.localidades_navarra, R.array.localidades_orense,
+                R.array.localidades_palencia, R.array.localidades_pontevedra, R.array.localidades_salamanca,
                 R.array.localidades_santaCruzDeTenerife, R.array.localidades_segovia, R.array.localidades_sevilla, R.array.localidades_soria,
                 R.array.localidades_tarragona, R.array.localidades_teruel, R.array.localidades_toledo, R.array.localidades_valencia,
                 R.array.localidades_valladolid, R.array.localidades_vizcaya, R.array.localidades_zamora, R.array.localidades_zaragoza};
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, localidades[position], android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spLocalidad.setAdapter(adapter);
+        String ele = (String) parent.getItemAtPosition(position);
+        Toast.makeText(getApplicationContext(), getString(R.string.tmMensajeSELECCIONADO) + ele, Toast.LENGTH_LONG).show();
 
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
+        Toast.makeText(getApplicationContext(), getString(R.string.tmNOSeleccion), Toast.LENGTH_LONG).show();
     }
 }
