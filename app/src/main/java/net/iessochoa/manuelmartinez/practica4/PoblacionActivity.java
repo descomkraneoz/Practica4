39c187486e5f60923679e4ae03f44ec91ae6b897
ManuelM <descom666999@gmail.com>
2019-10-23 16:11:51 +0200

    mejora en el spinner de localidad

--- app/src/main/java/net/iessochoa/manuelmartinez/practica4/PoblacionActivity.java
+++ app/src/main/java/net/iessochoa/manuelmartinez/practica4/PoblacionActivity.java
@@ -2,6 +2,7 @@ package net.iessochoa.manuelmartinez.practica4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -46,59 +47,27 @@ public class PoblacionActivity extends AppCompatActivity implements AdapterView.
         spProvincia.setAdapter(adapterProvincia);
         spProvincia.setOnItemSelectedListener(this);
 
-
-
-        /*spProvincia.setOnItemSelectedListener(
-                new AdapterView.OnItemSelectedListener() {
-                    public void onItemSelected(AdapterView<?> parent,
-                                               android.view.View v, int position, long id) {
-                        String ele=(String) parent.getItemAtPosition(position);
-                        Toast.makeText(getApplicationContext(),getString(R.string.tmMensajeSELECCIONADO)+ ele,Toast.LENGTH_LONG).show();
-                    }
-
-                    public void onNothingSelected(AdapterView<?> parent) {
-                        Toast.makeText(getApplicationContext(),getString(R.string.tmNOSeleccion),Toast.LENGTH_LONG).show();
-                    }
-                });*/
-
-        /*ArrayAdapter<CharSequence> adapterLocalidad=ArrayAdapter.createFromResource(this,
-                R.array.array_provincia_a_localidades,
-                android.R.layout.simple_spinner_item);
-        adapterLocalidad.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        spLocalidad.setAdapter(adapterLocalidad);
-        spLocalidad.setOnItemSelectedListener(
-                new AdapterView.OnItemSelectedListener() {
-                    public void onItemSelected(AdapterView<?> parent,
-                                               android.view.View v, int position, long id) {
-                        String ele=(String) parent.getItemAtPosition(position);
-                        Toast.makeText(getApplicationContext(),getString(R.string.tmMensajeSELECCIONADO)+ ele,Toast.LENGTH_LONG).show();
-                    }
-
-                    public void onNothingSelected(AdapterView<?> parent) {
-                        Toast.makeText(getApplicationContext(),getString(R.string.tmNOSeleccion),Toast.LENGTH_LONG).show();
-                    }
-                });*/
-
     }
+
+    /**
+     * Sobreescribimos el método onItemSelected para poder asociar los spiner entre si
+     */
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        int[] localidades = {R.array.localidades_albacete, R.array.localidades_alava, R.array.localidades_alicante, R.array.localidades_almeria,
-                R.array.localidades_asturias, R.array.localidades_avila, R.array.localidades_badajoz, R.array.localidades_barcelona,
-                R.array.localidades_burgos, R.array.localidades_caceres, R.array.localidades_cadiz, R.array.localidades_cantabria,
-                R.array.localidades_castellon, R.array.localidades_ceuta, R.array.localidades_ciudadReal, R.array.localidades_cordoba,
-                R.array.localidades_cuenca, R.array.localidades_gerona, R.array.localidades_granada, R.array.localidades_guadalajara,
-                R.array.localidades_guipuzcoa, R.array.localidades_huelva, R.array.localidades_huesca, R.array.localidades_islasBaleares,
-                R.array.localidades_jaen, R.array.localidades_laCoruna, R.array.localidades_laRioja, R.array.localidades_lasPalmas,
-                R.array.localidades_lerida, R.array.localidades_leon, R.array.localidades_lugo, R.array.localidades_madrid, R.array.localidades_malaga,
-                R.array.localidades_melilla, R.array.localidades_murcia, R.array.localidades_navarra, R.array.localidades_orense,
-                R.array.localidades_palencia, R.array.localidades_pontevedra, R.array.localidades_salamanca,
-                R.array.localidades_santaCruzDeTenerife, R.array.localidades_segovia, R.array.localidades_sevilla, R.array.localidades_soria,
-                R.array.localidades_tarragona, R.array.localidades_teruel, R.array.localidades_toledo, R.array.localidades_valencia,
-                R.array.localidades_valladolid, R.array.localidades_vizcaya, R.array.localidades_zamora, R.array.localidades_zaragoza};
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, localidades[position], android.R.layout.simple_spinner_dropdown_item);
+        //creamos el array que almacene las localidades
+        TypedArray arrayLocalidades;
+        //Dotamos al array del contenido
+        arrayLocalidades = getResources().obtainTypedArray(R.array.array_provincia_a_localidades);
+        //Obtenemos las localidades mediante la posición del elemento seleccionado en Provincias
+        CharSequence[] localidades = arrayLocalidades.getTextArray(position);
+
+        //Creamos el adaptador y lo dotamos con los recursos
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item, localidades);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spLocalidad.setAdapter(adapter);
+        //Mensaje que informa sobre la localidad seleccionada
         String ele = (String) parent.getItemAtPosition(position);
         Toast.makeText(getApplicationContext(), getString(R.string.tmMensajeSELECCIONADO) + ele, Toast.LENGTH_LONG).show();
 
